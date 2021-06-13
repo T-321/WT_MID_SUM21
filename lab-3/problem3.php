f278513fe54fd7b9b2fc535b19c23a16664cbdcd
Tonni123 <tabassumtonni977@gmail.com>
2021-06-13 15:57:22 +0600

    lab3 done

---
+++ lab-3/problem3.php
@@ -0,0 +1,14 @@
+<?php
+$length=7;
+$width=7;
+
+$perimeter= 2*($length+$width);
+echo "Perimeter: $perimeter<br>";
+
+$area= ($length*$width);
+echo "Area: $area<br>";
+
+if($length===$width){
+echo "the shape is a square";
+}
+?>
---
+++ lab-3/problem4.php
@@ -0,0 +1,11 @@
+<?php
+$name="mina";
+
+if($name=="raju" or $name=="mina" or $name=="mithu"){
+echo "Hello!";
+}
+else
+{
+echo "You are not recognized";
+}
+?>
